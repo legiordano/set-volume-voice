26645fc06ddd9988c14bf721e157807fd7bcc4a3
legiordano <lucasgiordano2219@gmail.com>
2023-07-26 17:15:44 -0300

    Add logs

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import speech_recognition as sr
-import osascript  # Solo para macOS
+import osascript
+import logging
 
 def set_system_volume(volume):
-    # Ajustar el volumen del sistema utilizando osascript (para macOS)
     osascript.osascript(f'set volume output volume {volume}')
 
 def main():
@@ -15,22 +15,21 @@ def main():
                 recognizer.adjust_for_ambient_noise(source)
                 audio = recognizer.listen(source)
 
-                # Reconocer el comando de voz
                 command = recognizer.recognize_google(audio).lower()
 
                 if "subir volumen" in command:
-                    set_system_volume(50)  # Ajusta el volumen deseado aquí (por ejemplo, 50).
+                    set_system_volume(50)
                 elif "bajar volumen" in command:
-                    set_system_volume(10)  # Ajusta el volumen deseado aquí (por ejemplo, 10).
+                    set_system_volume(10)
                 elif "terminar" in command:
                     break
                 else:
                     print("Comando no reconocido.")
 
             except sr.UnknownValueError:
-                print("No se pudo entender el comando de voz.")
+                logging.error("No se pudo entender el comando de voz.")
             except sr.RequestError:
-                print("Error al solicitar el servicio de reconocimiento de voz.")
+                logging.error("Error al solicitar el servicio de reconocimiento de voz.")
 
 if __name__ == "__main__":
     main()
